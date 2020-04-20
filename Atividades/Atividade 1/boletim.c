ff9c668f6f66316d7c3cb7367b6338c5d8c543c0
leticia <lelealvesaz@gmail.com>
2020-04-20 20:02:21 -0300

    Atividade 1

---
+++ Atividades/Atividade 1/boletim.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char const *argv[]){
+	double notas[10][3];
+	int i;
+	float media;
+	
+	for(i==0;i<10;i++){
+		printf("Digite a primeira nota dos alunos:", notas[i][0]);
+		scanf("%d", &notas[i][0]);
+	    printf("Digite a segunda nota dos alunos:", notas[i][1]);
+		scanf("%d", &notas[i][1]);
+	}
+	
+	
+	notas[i][2]=(notas[i][0]+notas[i][1])/2;
+	media=notas[i][2];
+	
+    for(i=0;i<10;i++){
+    	printf("Primeria nota: %d\n", notas[i][0]);
+    	printf("Segunda nota:%d\n", notas[i][1]);
+	    printf("A media é: %d\n", notas[i][2]);
+	}
+
+	
+
+}
