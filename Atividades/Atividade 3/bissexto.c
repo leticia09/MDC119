b0097a1ab26f46dbb4b46bc180c3821119ec118e
leticia <lelealvesaz@gmail.com>
2020-04-22 05:16:10 -0300

    Atividades

--- Atividades/Atividade 3/bissexto.c
+++ Atividades/Atividade 3/bissexto.c
@@ -1,2 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int is_bissexto(int i){
+    int ano;
+
+if (ano % 4 == 0 && (ano % 400 == 0 || ano % 100 != 0)) {
+    printf("\n O ano %d e bissexto.\n", ano);
+  }
+  
+  else {
+    printf("\nO ano %d não e bissexto.\n", ano);
+  }
+}
+
+int main(int arc, char *argv[]){
+int ano;
+
+printf("Digite o ano:\n");
+scanf("%d", & ano);
+
+is_bissexto(ano);
+
+}
