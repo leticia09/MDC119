cc9928d0651c1ba8501866932857d8fd5fc01f9d
leticia <lelealvesaz@gmail.com>
2020-04-22 06:14:26 -0300

    atividade

--- Atividades/Atividade 3/conversorhoras.c
+++ Atividades/Atividade 3/conversorhoras.c
@@ -1,2 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int converte_hhmmss_em_segundos(int h, int m, int s){
+   s=18900;
+   h=s/60/60;
+   m=s/60;
+}
+int main(){
+
+ printf("O resultado em horas, minutos e segundos é : %i : %i : %i" converte_hhmmss_em_segundos(h, m, s));
+
+}
